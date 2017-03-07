44689dc639dac1d270fffb19f42e8345212376bc
1103834619@qq.com <zjh123456>
2017-03-07 09:28:03 +0800

    代码提交

---
+++ LYSAutoScrollView/LYSAutoScrollView/LYSNoticeView.h
@@ -0,0 +1,13 @@
+//
+//  LYSNoticeView.h
+//  LYSAutoScrollView
+//
+//  Created by jk on 2017/3/7.
+//  Copyright © 2017年 Goldcard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYSNoticeView : UIView
+
+@end
---
+++ LYSAutoScrollView/LYSAutoScrollView/LYSNoticeView.m
@@ -0,0 +1,17 @@
+//
+//  LYSNoticeView.m
+//  LYSAutoScrollView
+//
+//  Created by jk on 2017/3/7.
+//  Copyright © 2017年 Goldcard. All rights reserved.
+//
+
+#import "LYSNoticeView.h"
+
+@interface LYSNoticeView ()
+
+@end
+
+@implementation LYSNoticeView
+
+@end
